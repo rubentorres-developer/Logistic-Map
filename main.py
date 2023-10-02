6637036fbf39b844edbb1640da18d06d4fb36a62
rubentorres-developer <rubentorres.developer@outlook.com>
2023-10-02 19:36:52 +0100

    refactoring

--- main.py
+++ main.py
@@ -13,37 +13,42 @@ def logistic(r, x):
 def get_x_values(r):
     x_values = np.zeros(MAX_ITERATIONS + 1)
     x_values[0] = STARTING_X
-    for i in range(MAX_ITERATIONS):
+    for i in np.arange(MAX_ITERATIONS):
         x_values[i + 1] = logistic(r, x_values[i])
     return x_values
 
 
-def get_deltas(res):
-    deltas = []
-    for i in range(len(res) - 1):
-        d = res[i + 1] - res[i]
-        deltas.append(d)
+def get_deltas(x_values):
+    deltas = np.zeros(MAX_ITERATIONS)
+    for i in np.arange(MAX_ITERATIONS - 1):
+        deltas[i] = x_values[i + 1] - x_values[i]
     return deltas
 
 
 def question_a():
     for r in R_VALUES:
         x_values = get_x_values(r)
-        it = [y for y in range(len(x_values))]
+        iterations_x = np.arange(x_values.size)
         plt.title(f"Value of X against Iterations (r = {r})")
-        plt.plot(it, x_values)
+        plt.plot(iterations_x, x_values, color="r")
+        plt.xlabel("Iterations")
+        plt.ylabel("X")
         plt.show()
 
         deltas = get_deltas(x_values)
-        it2 = [y for y in range(len(deltas))]
+        iterations_delta = np.arange(deltas.size)
         plt.title(f"Value of Deltas against Iterations (r = {r})")
-        plt.plot(it2, deltas)
+        plt.plot(iterations_delta, deltas, color="g")
+        plt.xlabel("Iterations")
+        plt.ylabel("Deltas")
         plt.show()
 
         successors = x_values[1:]
         predecessors = x_values[:-1]
         plt.title(f"Value of Successors against Predecessors (r = {r})")
-        plt.plot(predecessors, successors)
+        plt.xlabel("Predecessors")
+        plt.ylabel("Successors")
+        plt.plot(predecessors, successors, color="b")
         plt.show()
 
         plt.close()
